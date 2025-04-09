835b6d1ec3343ba96792ed166de25b5e455cc911
Elliot Sayes <elliot@sayes.cloud>
2025-04-09 17:24:45 +0200

    Revert NaN canonicalization in aot_emit_conversion.c and aot_emit_numberic.c due to performance reasons.

--- core/iwasm/compilation/aot_emit_conversion.c
+++ core/iwasm/compilation/aot_emit_conversion.c
@@ -8,7 +8,6 @@
 #include "aot_emit_numberic.h"
 #include "../aot/aot_intrinsic.h"
 #include "../aot/aot_runtime.h"
-#include "aot_llvm.h"
 
 static LLVMValueRef
 call_fcmp_intrinsic(AOTCompContext *comp_ctx, AOTFuncContext *func_ctx,
@@ -753,10 +752,6 @@ aot_compile_op_f32_demote_f64(AOTCompContext *comp_ctx,
         return false;
     }
 
-    if (comp_ctx->enable_nan_canonicalization) {
-        res = aot_canonicalize_nan(comp_ctx, func_ctx, res, true);
-    }
-
     PUSH_F32(res);
     return true;
 fail:
@@ -864,10 +859,6 @@ aot_compile_op_f64_promote_f32(AOTCompContext *comp_ctx,
     if (!res) {
         aot_set_last_error("llvm build conversion failed.");
         return false;
-    }
-
-    if (comp_ctx->enable_nan_canonicalization) {
-        res = aot_canonicalize_nan(comp_ctx, func_ctx, res, false);
     }
 
     PUSH_F64(res);
